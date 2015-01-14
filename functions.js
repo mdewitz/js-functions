cfb595da04eb9ea30ff4fe29d61f5a2454eaaebe
Mika <mikadewitz@gmail.com>
2015-01-13 18:21:58 -1000

    returns square of given value

--- functions.js
+++ functions.js
@@ -66,7 +66,9 @@ function divide(x,y){
  * @param {number} x, number to be squared
  * @return {number} squared
  */
-
+function square (x){
+  return (x, x*x);
+}
 /**
  * Performs a mathematical operation on two numbers.
  * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
